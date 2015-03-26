3cbf6a525841ae87aada3b9a74f5dc68854ebe41
Adam Stewart <iwebsource1@gmail.com>
2015-03-25 22:35:43 -0300

    Disabled liblight. Calls from Device Instead.

--- msm8974/Android.mk
+++ msm8974/Android.mk
@@ -1,4 +1,4 @@
-display-hals := libgralloc libgenlock libcopybit liblight
+display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 display-hals += libmemtrack
 ifeq ($(call is-vendor-board-platform,QCOM),true)
